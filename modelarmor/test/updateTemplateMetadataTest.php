6e1c4da7e42057fa42f3e189a3f858910c7412fc
Meet Patel3 <meet.patel3@CDSYS.LOCAL>
2025-05-26 12:08:35 +0530

    Added code samples to update model armor templates

---
+++ modelarmor/test/updateTemplateMetadataTest.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright 2025 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\ModelArmor;
+
+use Google\Cloud\ModelArmor\V1\FilterConfig;
+use Google\Cloud\ModelArmor\V1\Template;
+use Google\Cloud\ModelArmor\V1\CreateTemplateRequest;
+use Google\Cloud\ModelArmor\V1\Template\TemplateMetadata;
+
+class updateTemplateMetadataTest extends BaseTestCase
+{
+    protected static function getTemplatePrefix(): string
+    {
+        return 'php-update-template-metadata-';
+    }
+
+    private static function createTemplate(string $projectId, string $locationId, string $templateId): void
+    {
+        $templateMetadata = (new TemplateMetadata())
+            ->setLogSanitizeOperations(false);
+
+        $template = new Template()
+            ->setFilterConfig(new FilterConfig())
+            ->setTemplateMetadata($templateMetadata);
+
+        $request = (new CreateTemplateRequest())
+            ->setParent("projects/{$projectId}/locations/{$locationId}")
+            ->setTemplateId($templateId)
+            ->setTemplate($template);
+
+        self::$client->createTemplate($request);
+    }
+
+    public function testUpdateTemplateMetadata()
+    {
+        $projectId = self::getProjectId();
+
+        // Create template before updating it.
+        self::createTemplate($projectId, self::$locationId, self::$templateId);
+
+        $output = $this->runSnippetfile('update_template_metadata', [
+            $projectId,
+            self::$locationId,
+            self::$templateId,
+        ]);
+
+        $expectedTemplateString = 'Template updated: projects/' . $projectId . '/locations/' . self::$locationId . '/templates/' . self::$templateId;
+        $this->assertStringContainsString($expectedTemplateString, $output);
+    }
+}
