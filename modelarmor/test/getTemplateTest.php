6a2432170e2debf178f4303c46c21d57680dc885
Meet Patel3 <meet.patel3@CDSYS.LOCAL>
2025-05-26 11:21:51 +0530

    Added code samples for delete, get and list model armor templates

---
+++ modelarmor/test/getTemplateTest.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Copyright 2025 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\ModelArmor;
+
+use Google\Cloud\ModelArmor\V1\CreateTemplateRequest;
+use Google\Cloud\ModelArmor\V1\FilterConfig;
+use Google\Cloud\ModelArmor\V1\Template;
+
+class getTemplateTest extends BaseTestCase
+{
+    protected static function getTemplatePrefix(): string
+    {
+        return 'php-get-template-';
+    }
+
+    private static function createTemplate(string $projectId, string $locationId, string $templateId): void
+    {
+        $template = new Template()->setFilterConfig(new FilterConfig());
+
+        // Set template properties here if needed.
+        $request = (new CreateTemplateRequest())
+            ->setParent("projects/{$projectId}/locations/{$locationId}")
+            ->setTemplateId($templateId)
+            ->setTemplate($template);
+
+        self::$client->createTemplate($request);
+    }
+
+    public function testGetTemplate()
+    {
+        $projectId = self::getProjectId();
+
+        // Create template before retrieving it.
+        self::createTemplate($projectId, self::$locationId, self::$templateId);
+
+        $output = $this->runSnippetfile('get_template', [
+            $projectId,
+            self::$locationId,
+            self::$templateId,
+        ]);
+
+        $expectedTemplateString = 'Template retrieved: projects/' . $projectId . '/locations/' . self::$locationId . '/templates/' . self::$templateId;
+        $this->assertStringContainsString($expectedTemplateString, $output);
+    }
+}
